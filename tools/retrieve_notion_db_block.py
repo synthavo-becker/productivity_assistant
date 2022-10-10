6519459c6dfa2f0f981a0d69b7ae3441eaec7842
Paul Becker <wi19041@lehre.dhbw-stuttgart.de>
2022-10-10 14:58:42 +0200

    added file to retrieve block

---
+++ tools/retrieve_notion_db_block.py
@@ -0,0 +1,30 @@
+import requests
+import json 
+
+database_id = "326c3d22565c4473982d6f935e28cfff"
+
+url = f"https://api.notion.com/v1/databases/{database_id}/query"
+
+payload = {"page_size": 200}
+token = "secret_9ml5fh1fi8KEKffPhwRLfOMEbJldRrQp61LRVwZTDFo"
+headers = {
+    "Accept": "application/json",
+    "Authorization": "Bearer " + token,
+    "Notion-Version": "2022-02-22",
+    "Content-Type": "application/json"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+json_loaded = json.loads(response.text)
+
+x = json_loaded['results'][0]['properties']['Task']['title'][0]['text']
+y = [json_loaded['results'][t]['properties']['Task']['title'][0]['text']["content"] for t in range(len(json_loaded["results"])-1)]
+#print(x)
+
+searched_string = "Urlaubs"
+for index, item in enumerate(y):
+    if searched_string in item:
+        print("HIIIIER")
+        print(index)
+print("ji")
